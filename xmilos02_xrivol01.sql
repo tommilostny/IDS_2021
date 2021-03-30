dbebcf7b2e759fc3079865864a6da24f5f7d0849
Tom Milostny <tommilostny@live.com>
2021-03-30 11:15:02 +0200

    Initial commit

---
+++ xmilos02_xrivol01.sql
@@ -0,0 +1,22 @@
+-- Kavárna (ID, Název, Adresa, Otevírací hodiny, Kapacita míst, Popis)
+
+
+-- Uživatel (ID, Jméno, E-mail, Oblíbený druh přípravy, Vypitých káv denně)
+
+
+-- Zaměstnanec (ID, Jméno, Pracovní pozice)
+
+
+-- Recenze (ID, Text recenze, Počet hvězdiček, Datum návštěvy)
+
+
+-- Reakce (ID, Text reakce, Datum, Počet palců nahoru, Počet palců dolů)
+
+
+-- Druh kávy (ID, Název, Oblast původu, Kvalita, Popis, Standardní cena)
+
+
+-- Kávová zrna (ID, Odrůda, Stupeň kyselosti, Aroma, Chuť)
+
+
+-- Cupping akce (ID, Název, Datum konání, Počet volných míst)
